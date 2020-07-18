2387af95157ec58fe118348e8526d91e86e7e855
Kerri McMahon <47372452+kmcmahon4@users.noreply.github.com>
2020-07-17 20:58:29 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+## iMessage Bot
+A simple script that will read a text file and send the contents to someone via iMessage.
+
+#### Usage
+```
+node imessagebot.js file_name phone_number
+```
+
+You can use [raven.txt](https://github.com/chaotea/imessage-bot/blob/master/raven.txt) to try it out!
---
+++ imessagebot.js
@@ -0,0 +1,37 @@
+const imessage = require("osa-imessage")
+const fs = require("fs")
+
+if (process.argv.length != 4) {
+    console.log("Usage: node imessagebot.js file_name phone_number")
+    process.exit(1)
+} else if (!(process.argv[2].endsWith(".txt"))) {
+    console.log("Error: file must have the .txt extension")
+    process.exit(1)
+} else if (process.argv[3].length != 12) {
+    console.log("Error: phone number must be 10 digits, e.g. +11234567890")
+    process.exit(1)
+}
+
+var file_name = process.argv[2]
+var phone_number = process.argv[3]
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+
+async function send(lines) {
+    for (let i = 0; i < lines.length; i++) {
+        imessage.send('+16315229248', lines[i])
+        console.log("Sent: " + lines[i])
+        await sleep(1000)
+    }
+}
+
+fs.readFile(file_name, "utf8", function(err, data) {
+    if (err) throw err
+    console.log("OK: " + file_name)
+    console.log("Sending...")
+    lines = data.split(/\n/)
+    send(lines)
+})
